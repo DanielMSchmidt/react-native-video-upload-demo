6d3b5977dbee0bc56371465cc4a75dc4af2783fc
Daniel Schmidt <dschmidt@mesosphere.com>
2018-06-22 20:07:02 +0200

    move * into videos folder

--- README.md
+++ README.md
@@ -4,12 +4,18 @@ This is a project demoing how to upload videos to a node express web server.
 
 ## Status
 
+### In `videos/`
+
 - [x] Video Uploading
   - [x] Backend
   - [x] Frontend
-- [ ] Stream Download
+- [x] Stream Download
   - [x] [Backend](https://medium.com/@daspinola/video-stream-with-node-js-and-html5-320b3191a6b6)
-  - [ ] Frontend
+  - [x] Frontend
+
+
+### In `streams/`
+
 - [ ] Stream Upload
   - [ ] Backend
   - [ ] Frontend
--- videos/frontend/android/app/src/main/java/com/frontend/MainApplication.java
+++ videos/frontend/android/app/src/main/java/com/frontend/MainApplication.java
@@ -3,7 +3,6 @@ package com.frontend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.brentvatne.react.ReactVideoPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactVideoPackage(),
             new RNCameraPackage()
       );
     }
